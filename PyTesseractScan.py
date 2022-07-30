9704759ecb0468067f98088920ed25c569e3dd11
Van Francisco <vanfrancisco24@gmail.com>
2022-07-31 01:46:00 +0800

    UPDATED SYNTAX

--- PyTesseractScan.py
+++ PyTesseractScan.py
@@ -5,14 +5,14 @@ from pynput import keyboard
 import time
 Clear = True
 def Image2Text(imag):
-    ExtractedText = pytesseract.image_to_string(imag, config=("--psm 10"))
+    ExtractedText = pytesseract.image_to_string(imag, config=("--psm 7"))
     return ExtractedText
 
 def Grayscale(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 def NoiseRemove(img):
-    return cv2.blur(img, (1,1))
+    return cv2.blur(img, (1,3))
 
 def thresholding(img):
     return cv2.threshold(img, 0 , 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
@@ -27,21 +27,28 @@ def StartGame():
 # Working Resolution: 1280 x 1024
 def StartTyping():
     while True:
-        GrabImg = ImageGrab.grab((250,490,900,540))
+        GrabImg = ImageGrab.grab((400,490,850,540))
         GrabImg.save('SSImage.jpg')
         # GrabImg.show()
         image = cv2.imread('SSImage.jpg')
         image = Grayscale(image)
-        image = thresholding(image)
+        # image = thresholding(image)
         image = NoiseRemove(image)
         res = Image2Text(image)
+        if (res.replace(" ","").lower().lstrip().rstrip() == "qaa=e"):
+            res = res.replace("a","&",1)
+        elif(res.replace(" ","").lower().lstrip().rstrip() == "astrogir]"):
+            res = res.replace("]","l",1)
         for i in res.replace(" ","").lower().lstrip().rstrip():
+            if (i == "’"):
+                i = "'"
             keyboard.Controller().press(i)
             keyboard.Controller().release(i)
-            time.sleep(0.03)
+            time.sleep(0.001)
         print(res.replace(" ","").lower().lstrip().rstrip())
         print(len(res.replace(" ","").lower().lstrip().rstrip()))
 
+#3 seconds to change to the game
 time.sleep(3)
 StartGame()
 time.sleep(5)
---
+++ testscript.py
@@ -0,0 +1,3 @@
+res = "qaa=e"
+res = res.replace("a","&",1)
+print(res)
